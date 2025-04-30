71a57aee7b66cbd972687455b5c72e6f097631bb
tinntt0304 <trungtin.nguyen34@gmail.com>
2025-04-30 23:52:30 +0700

    ok

---
+++ src/components/Footer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-[#6DD19C] text-white py-8">
+      <div className="container mx-auto px-4">
+        <div className="grid md:grid-cols-3 gap-8">
+          <div>
+          <a href="main.tsx" className="logo">
+          <img src="/logo_trang.png" alt="Giao Rẻ Hơn"></img>
+            </a>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold mb-4">Liên hệ</h3>
+            <p className="text-white-200">
+              Địa chỉ: 36/52/5 Nguyễn Gia Trí, Phường 25, Quận Bình Thạnh,Thành phố Hồ Chí Minh<br />
+              Email: cskh@giaorehon.vn<br />
+              Hotline: 035.695.4034
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl font-bold mb-4">Theo dõi chúng tôi</h3>
+            <div className="flex space-x-4 mb-4">
+              <a href="https://www.facebook.com/profile.php?id=61568192883725" className="text-blue-200 hover:text-white">Facebook</a>
+              <a href="#" className="text-blue-200 hover:text-white">LinkedIn</a>
+              <a href="#" className="text-blue-200 hover:text-white">Twitter</a>
+            </div>
+          </div>
+        </div>
+        <div className="border-t border-white-800 mt-6 pt-6">
+          <div className="flex flex-wrap gap-4 justify-center text-sm text-blue-200">
+            <a href="/privacy-policy" className="hover:text-white">Chính sách bảo mật</a>
+            <span>•</span>
+            <a href="/terms" className="hover:text-white">Điều khoản sử dụng</a>
+            <span>•</span>
+            <a href="/payment-policy" className="hover:text-white">Chính sách thanh toán</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/PrivacyPolicy.tsx
@@ -0,0 +1,227 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6 text-center">CHÍNH SÁCH BẢO MẬT</h1>
+      
+      <div className="prose max-w-none">
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">I.Giải thích</h2>
+          <p className="text-black 200">
+            Chính Sách Bảo Mật này quy định các chính sách và thủ tục của Chúng tôi về việc thu thập, sử dụng và tiết lộ
+thông tin của Quý khách khi Quý khách sử dụng Dịch vụ, đồng thời thông báo cho Quý khách về các quyền
+riêng tư của mình cũng như cách thức pháp luật bảo vệ Quý khách. </p>
+<p className="text-black 200">Chúng tôi sử dụng Dữ liệu cá nhân của Quý khách để cung cấp và cải thiện Dịch vụ. Bằng việc sử dụng Dịch vụ,
+Quý khách đồng ý với việc thu thập và sử dụng thông tin theo Chính Sách Bảo Mật này.</p>
+         
+     
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">II.Định nghĩa</h2>
+          <p className="mb-4 text-black 200">
+          Trong phạm vi của Chính Sách Bảo Mật này:
+          </p>
+          <ul className="list-disc pl-6 mb-4">
+            <li>"Tài khoản" là tài khoản duy nhất được tạo cho Quý khách để truy cập Dịch vụ của chúng tôi hoặc một
+            phần của Dịch vụ.</li>
+            <li>"Công ty liên kết" là tổ chức kiểm soát, bị kiểm soát hoặc cùng chịu sự kiểm soát với một bên, trong đó
+"kiểm soát" được hiểu là quyền sở hữu từ 50% trở lên cổ phần, quyền lợi vốn hoặc các chứng khoán khác
+có quyền biểu quyết bầu giám đốc hoặc cơ quan quản lý tương đương.</li>
+            <li>"Ứng dụng" là phần mềm Giao Rẻ Hơn do Công ty TNHH Giao Rẻ Hơn cung cấp.</li>
+            <li>"Công ty" (gọi là "Công ty", "Chúng tôi", "Của chúng tôi" trong Thỏa thuận này) là Công ty TNHH Giao
+            Rẻ Hơn, địa chỉ: 36/52/5 Nguyễn Gia Trí, Phường 25, Quận Bình Thạnh, TP. Hồ Chí Minh, Việt Nam.</li>
+            <li>"Quốc gia" là Việt Nam.</li>
+            <li>"Thiết bị" là bất kỳ thiết bị nào có thể truy cập Dịch vụ như máy tính, điện thoại di động hoặc máy tính
+            bảng.</li>
+            <li>"Dữ liệu cá nhân" là bất kỳ thông tin nào liên quan đến một cá nhân đã xác định hoặc có thể xác định
+            được.</li>
+            <li>"Dịch vụ" là Ứng dụng.</li>
+            <li>"Nhà cung cấp dịch vụ" là cá nhân hoặc pháp nhân xử lý dữ liệu thay mặt cho Công ty, bao gồm các công
+            ty hoặc cá nhân bên thứ ba được Công ty thuê để hỗ trợ, cung cấp hoặc phân tích Dịch vụ.</li>
+            <li>"Dịch vụ mạng xã hội bên thứ ba" là bất kỳ trang web hoặc mạng xã hội nào mà Người dùng có thể đăng
+            nhập hoặc tạo tài khoản để sử dụng Dịch vụ.</li>
+            <li>"Dữ liệu sử dụng" là dữ liệu được thu thập tự động, phát sinh từ việc sử dụng Dịch vụ hoặc từ chính hạ
+            tầng Dịch vụ (ví dụ: thời lượng truy cập trang).</li>
+            <li>"Quý khách" là cá nhân truy cập hoặc sử dụng Dịch vụ, hoặc tổ chức, pháp nhân mà cá nhân đó đại diện
+            khi truy cập hoặc sử dụng Dịch vụ.</li>
+          </ul>
+        </section>
+
+        <section className="mb-8">
+          <h2 className="text-2xl font-semibold mb-4">III.Thu thập và Sử dụng Dữ liệu Cá nhân của Quý khách</h2>
+          <p className="mb-4 text-black font-bold">
+          Loại Dữ liệu được Thu thập
+          </p>
+          <p className="text-black">Khi sử dụng Dịch vụ của Chúng tôi, Chúng tôi có thể yêu cầu Quý khách cung cấp một số thông tin nhận
+dạng cá nhân có thể được sử dụng để liên hệ hoặc nhận diện Quý khách, bao gồm nhưng không giới hạn
+ở:</p>
+          <ul className="list-disc pl-6 mb-4">
+            <li>Địa chỉ email</li>
+            <li>Họ và tên</li>
+            <li>Số điện thoại</li>
+            <li>Địa chỉ, tỉnh/thành phố, mã bưu chính</li>
+            <li>Thông tin thanh toán, sao kê dịch vụ</li>
+            <li>Dữ liệu sử dụng</li>
+          </ul>
+          <p className="mb-4 text-black font-bold">
+          Dữ liệu sử dụng
+          </p>
+          <p className="text-black">Dữ liệu sử dụng được thu thập tự động khi sử dụng Dịch vụ và có thể bao gồm: địa chỉ IP của Thiết bị, loại trình
+duyệt, phiên bản trình duyệt, các trang đã truy cập, thời gian và ngày truy cập, thời gian sử dụng, mã định danh
+thiết bị và các dữ liệu chẩn đoán khác. Khi truy cập Dịch vụ qua thiết bị di động, Chúng tôi có thể thu thập thêm
+thông tin về loại thiết bị, mã định danh, hệ điều hành, loại trình duyệt di động, địa chỉ IP, mã định danh thiết bị
+và dữ liệu chẩn đoán liên quan.
+</p>
+<br />
+        <p className="mb-4 text-black font-bold">
+Thông tin từ Dịch vụ Mạng Xã hội Bên Thứ Ba (nếu có)
+          </p>
+          <p className="text-black">Công ty cho phép Quý khách tạo tài khoản và đăng nhập qua các Dịch vụ Mạng Xã hội Bên Thứ Ba (nếu có)
+như Google, Facebook, Instagram, Twitter, LinkedIn. Nếu Quý khách đăng ký hoặc cho phép truy cập, Chúng
+tôi có thể thu thập dữ liệu cá nhân đã liên kết với tài khoản đó (tên, email, hoạt động, danh bạ, v.v.). Quý khách
+có thể lựa chọn chia sẻ thêm thông tin với Công ty thông qua các tài khoản này. Khi cung cấp thông tin đó, Quý
+khách đồng ý để Công ty sử dụng, chia sẻ và lưu trữ thông tin đó phù hợp với Chính Sách Bảo Mật này.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Thông tin thu thập khi sử dụng Ứng dụng
+          </p>
+          <p className="text-black">Khi sử dụng Ứng dụng, để cung cấp các tính năng, Chúng tôi có thể thu thập, với sự cho phép trước của Quý
+          khách:</p>
+          <ul className="list-disc pl-6 mb-4">
+            <li>Thông tin về vị trí của Quý khách</li>
+            <li>Thông tin từ danh bạ trên thiết bị</li>
+            <li>Hình ảnh và các thông tin khác từ camera hoặc thư viện ảnh của thiết bị</li>
+          </ul>
+          <p className="text-black">Thông tin này được sử dụng để cung cấp, cải thiện và cá nhân hóa Dịch vụ. Thông tin có thể được tải lên máy
+chủ của Công ty hoặc Nhà cung cấp dịch vụ, hoặc chỉ lưu trữ trên thiết bị của Quý khách. Quý khách có thể
+bật/tắt quyền truy cập thông tin này bất cứ lúc nào trong cài đặt thiết bị.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Mục đích Sử dụng Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Công ty có thể sử dụng Dữ liệu Cá nhân cho các mục đích sau:</p>
+          <ul className="list-disc pl-6 mb-4">
+            <li>Cung cấp và duy trì Dịch vụ, bao gồm việc giám sát việc sử dụng Dịch vụ.</li>
+            <li>Quản lý Tài khoản: quản lý đăng ký của Quý khách với tư cách là người dùng Dịch vụ.</li>
+            <li>Thực hiện hợp đồng: phát triển, thực hiện và hoàn thành hợp đồng mua bán sản phẩm, dịch vụ hoặc các
+            hợp đồng khác với Quý khách.</li>
+            <li>Liên hệ với Quý khách: qua email, điện thoại, SMS, Zalo hoặc các hình thức liên lạc điện tử khác về các
+            cập nhật, thông báo liên quan đến chức năng, sản phẩm, dịch vụ hoặc bảo mật.</li>
+            <li>Cung cấp thông tin, ưu đãi về hàng hóa, dịch vụ tương tự trừ khi Quý khách từ chối nhận thông tin.</li>
+            <li>Quản lý yêu cầu: giải quyết các yêu cầu của Quý khách.</li>
+            <li>Chuyển giao kinh doanh: sử dụng thông tin để đánh giá hoặc thực hiện sáp nhập, chuyển nhượng, tái cấu
+trúc, giải thể hoặc chuyển giao tài sản, trong đó Dữ liệu Cá nhân có thể là một phần tài sản được chuyển
+giao.</li>
+            <li>Mục đích khác: phân tích dữ liệu, xác định xu hướng sử dụng, đánh giá hiệu quả chiến dịch tiếp thị, cải
+            thiện Dịch vụ, sản phẩm, trải nghiệm người dùng.</li>
+          </ul>
+          <p className="mb-4 text-black font-bold">
+          Chia sẻ Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Công ty có thể chia sẻ Dữ liệu Cá nhân trong các trường hợp sau:</p>
+          <ul className="list-disc pl-6 mb-4">
+            <li>Với Nhà cung cấp dịch vụ: để cung cấp, giám sát, phân tích việc sử dụng Dịch vụ, liên hệ với Quý khách.</li>
+            <li>Chuyển giao kinh doanh: trong quá trình sáp nhập, bán tài sản, huy động vốn hoặc chuyển nhượng doanh
+            nghiệp.</li>
+            <li>Với Công ty liên kết: yêu cầu các bên liên kết tuân thủ Chính Sách Bảo Mật này.</li>
+            <li>Với đối tác kinh doanh: cung cấp sản phẩm, dịch vụ hoặc chương trình khuyến mại.</li>
+            <li>Với người dùng khác: khi Quý khách chia sẻ thông tin ở khu vực công cộng hoặc tương tác với người
+            dùng khác, thông tin có thể được công khai.</li>
+            <li>Theo sự đồng ý của Quý khách: cho các mục đích khác ngoài các trường hợp trên.</li>
+          </ul>
+          <p className="mb-4 text-black font-bold">
+          Lưu trữ Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Công ty sẽ lưu trữ Dữ liệu Cá nhân của Quý khách trong thời gian cần thiết cho các mục đích đã nêu trong
+Chính Sách Bảo Mật này, đồng thời tuân thủ các nghĩa vụ pháp lý, giải quyết tranh chấp và thực thi thỏa thuận.
+Dữ liệu sử dụng thường được lưu giữ trong thời gian ngắn hơn, trừ khi cần thiết để tăng cường bảo mật, cải thiện
+chức năng hoặc theo quy định pháp luật.</p>
+<br />
+          <p className="mb-4 text-black font-bold">
+          Chuyển giao Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Thông tin của Quý khách, bao gồm Dữ liệu Cá nhân, có thể được xử lý tại các văn phòng của Công ty hoặc tại
+các địa điểm khác nơi các bên liên quan thực hiện xử lý dữ liệu. Điều này có nghĩa dữ liệu có thể được chuyển
+đến và lưu trữ tại các máy chủ đặt ngoài khu vực pháp lý của Quý khách, nơi luật bảo vệ dữ liệu có thể khác
+biệt. Khi cung cấp thông tin, Quý khách đồng ý với việc chuyển giao này. Công ty sẽ thực hiện các biện pháp
+hợp lý để đảm bảo dữ liệu được bảo mật và chỉ chuyển giao khi có đủ kiểm soát an toàn phù hợp.</p>
+<br />
+          <p className="mb-4 text-black font-bold">
+          Xóa Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Quý khách có quyền yêu cầu xóa hoặc hỗ trợ xóa Dữ liệu Cá nhân mà Chúng tôi đã thu thập. Dịch vụ có thể cho
+phép Quý khách xóa thông tin từ trong Dịch vụ. Quý khách cũng có thể cập nhật, sửa đổi hoặc xóa thông tin bất
+cứ lúc nào bằng cách đăng nhập vào tài khoản và truy cập phần cài đặt tài khoản, hoặc liên hệ với Chúng tôi để
+yêu cầu truy cập, chỉnh sửa hoặc xóa thông tin. Tuy nhiên, Chúng tôi có thể cần giữ lại một số thông tin khi có
+nghĩa vụ pháp lý hoặc cơ sở hợp pháp.</p>
+<br />
+          <p className="mb-4 text-black font-bold">
+          Công bố Dữ liệu Cá nhân
+          </p>
+          <p className="mb-4 text-black font-semibold text-[14px]">
+          Giao dịch doanh nghiệp
+          </p>
+          <p className="text-black text-[14px]">Nếu Công ty tham gia sáp nhập, mua lại hoặc chuyển nhượng tài sản, Dữ liệu Cá nhân của Quý khách có thể
+được chuyển giao. Công ty sẽ thông báo trước khi dữ liệu được chuyển giao và chịu sự điều chỉnh của Chính
+Sách Bảo Mật mới.</p>
+<br />
+<p className="mb-4 text-black font-semibold text-[14px]">
+Thực thi pháp luật
+          </p>
+          <p className="text-black text-[14px]">Trong một số trường hợp nhất định, Công ty có thể bị yêu cầu tiết lộ Dữ liệu Cá nhân nếu luật pháp yêu cầu
+          hoặc theo yêu cầu hợp lệ của cơ quan nhà nước (ví dụ: tòa án, cơ quan quản lý).</p>
+          <br />
+<p className="mb-4 text-black font-semibold text-[14px]">
+Các yêu cầu pháp lý khác
+          </p>
+          <p className="text-black text-[14px]">Công ty có thể tiết lộ Dữ liệu Cá nhân khi tin tưởng một cách thiện chí rằng việc đó là cần thiết để:</p>
+          <ul className="list-disc pl-6 mb-4 text-[14px]">
+            <li>Tuân thủ nghĩa vụ pháp lý</li>
+            <li>Bảo vệ quyền và tài sản của Công ty</li>
+            <li>Ngăn chặn hoặc điều tra hành vi sai trái liên quan đến Dịch vụ</li>
+            <li>Bảo vệ an toàn cá nhân của người dùng hoặc cộng đồng</li>
+            <li>Phòng tránh trách nhiệm pháp lý</li>
+          </ul>
+          <p className="mb-4 text-black font-bold">
+          Bảo mật Dữ liệu Cá nhân
+          </p>
+          <p className="text-black">Công ty cam kết bảo vệ Dữ liệu Cá nhân của Quý khách, tuy nhiên không phương thức truyền tải nào qua
+Internet hoặc lưu trữ điện tử là an toàn tuyệt đối. Chúng tôi nỗ lực áp dụng các biện pháp bảo mật thương mại
+hợp lý nhưng không thể đảm bảo an toàn tuyệt đối cho dữ liệu.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Quyền riêng tư của trẻ em
+          </p>
+          <p className="text-black">Dịch vụ của Chúng tôi không hướng tới trẻ em dưới 13 tuổi. Chúng tôi không cố ý thu thập thông tin nhận dạng
+cá nhân từ trẻ em dưới 13 tuổi. Nếu Quý khách là phụ huynh hoặc người giám hộ và nhận thấy trẻ em đã cung
+cấp Dữ liệu Cá nhân, vui lòng liên hệ với Chúng tôi để thực hiện các biện pháp xóa thông tin. Nếu pháp luật yêu
+cầu sự đồng ý của phụ huynh, Chúng tôi có thể yêu cầu xác nhận trước khi thu thập và sử dụng thông tin.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Liên kết đến trang web khác
+          </p>
+          <p className="text-black">Dịch vụ có thể chứa liên kết đến các trang web không do Chúng tôi vận hành. Khi Quý khách nhấp vào liên kết
+bên thứ ba, Quý khách sẽ được chuyển đến trang web của bên đó. Chúng tôi khuyến nghị Quý khách xem xét
+Chính Sách Bảo Mật của từng trang web truy cập. Chúng tôi không kiểm soát và không chịu trách nhiệm về nội
+dung, chính sách bảo mật hoặc thực tiễn của bất kỳ trang web hoặc dịch vụ bên thứ ba nào.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Thay đổi Chính Sách Bảo Mật
+          </p>
+          <p className="text-black">Chúng tôi có thể cập nhật Chính Sách Bảo Mật này theo thời gian. Chúng tôi sẽ thông báo cho Quý khách về bất
+kỳ thay đổi nào bằng cách đăng Chính Sách Bảo Mật mới lên trang này, đồng thời gửi thông báo qua email
+và/hoặc thông báo nổi bật trên Dịch vụ trước khi thay đổi có hiệu lực. Ngày cập nhật sẽ được ghi rõ ở đầu Chính
+Sách Bảo Mật này. Quý khách nên thường xuyên kiểm tra để cập nhật các thay đổi.</p>
+<br />
+<p className="mb-4 text-black font-bold">
+Liên hệ
+          </p>
+          <p className="text-black">Nếu có bất kỳ thắc mắc nào về Chính Sách Bảo Mật này, Quý khách có thể liên hệ với Chúng tôi qua email:
+          cskh@giaorehon.vn</p>
+        </section>
+      </div>
+    </div>
+  );
+}
